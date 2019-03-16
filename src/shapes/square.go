301fa606f09116dc241a1a4af523d0a10ca3780b
An Doan <minh.an.doan@gmail.com>
2019-03-16 12:40:01 -0700

    Add code for the square shape.

---
+++ src/shapes/square.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type square float64
+
+func (s square) perimeter() float64 {
+	return 4 * float64(s)
+}
+
+func (s square) area() float64 {
+	return float64(s * s)
+}
+
+func main() {
+	var s square = 4.5
+	fmt.Println(s.perimeter())
+	fmt.Println(s.area())
+}
